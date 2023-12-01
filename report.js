233f33e325970f7d3eda3d52651a0f50e8204605
FariaRicha13 <fariahricha13@gmail.com>
2023-12-01 13:19:35 +0600

    upload report

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+require('dotenv').config();
+ 
+newman.run({
+    //collection: require('./collection/dmoney.json'),
+    collection:`https://api.postman.com/collections/31368683-b6b0851c-14c9-4884-8694-1ccb62ce579a?access_key=${process.env.accessKey}`,
+    //environment:require('./collection/env.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
